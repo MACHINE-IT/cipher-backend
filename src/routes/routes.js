9092236e0a47d7a2b70442b3cc6606e3fa91fa84
Yogesh Kumar <40662948+MACHINE-IT@users.noreply.github.com>
2023-12-11 13:01:55 +0530

    added .put in '/' to update sequence in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,10 @@
-import { addNewUser, getUserList } from "../controllers/Controllers.js"
+import { addNewUser, getUserList, updateSequence } from "../controllers/Controllers.js"
 
 
 const routes = (app) => {
     app.route('/')
         .get(getUserList)
+        .put(updateSequence)
         .post()
     app.route('/add-new-user')
         .post(addNewUser)
